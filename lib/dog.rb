4c9f06c0314c3793a414b4995fb2da30a9fc6538
steve <austines811@gmail.com>
2022-08-31 14:03:00 +0300

    commit tree 53a96d03a9caf754bf4bf27833692e51d7a24661

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,26 @@
-class Dog
-end
+#lets require this to aid in ORM
+require "active_record"
+
+#We tell it who we are writing to, and where they are
+ActiveRecord::Base.establish_connection(
+        adapter: "sqlite3",
+        database: "db/dogs.sqlite"
+    )
+
+#we create a table using the AR convention
+sql = <<-SQL
+CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+)
+SQL
+
+#lets execute SEEQUEL command
+ActiveRecord::Base.connection.execute(sql)
+
+#we then link a model to the DB table
+class Dog < ActiveRecord::Base
+end 
+
+
